c7b5c021020269db85e20aee751bb3bb691d2714
Lissy Gundersen <user@Users-MacBook-Pro.local>
2019-11-11 15:58:50 +0000

    completed

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,30 +1,53 @@
 class SortedList {
-  constructor() {
-    // ...
+  constructor(items, length) {
+    this.items = []
+    this.length = this.items.length
   }
 
-  add(item) {
-    // ...
+  add(x) {
+    this.items.push(x);
+    this.length++;
+
   }
 
-  get(pos) {
-    // ...
+  get(i) {
+    if (i <= this.length) {
+      return this.items[i]
+    } else {
+      throw new Error('OutOfBounds');
+    }
   }
 
   max() {
-    // ...
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return Math.max(...this.items);
+    }
   }
 
   min() {
-    // ...
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return Math.min(...this.items);
+    }
   }
 
   sum() {
-    // ...
+    let total = 0;
+    for (let i = 0; i < this.length; i++) {
+      total += this.items[i];
+    }
+    return total;
   }
 
   average() {
-    // ...
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.sum() / this.length;
+    }
   }
 }
 
